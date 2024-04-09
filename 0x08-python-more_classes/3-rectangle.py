411e448ad84ab56b9149c2bf4c5538acbd50d8c9
kgaogelo <kgaugelo.maile@gmail.com>
2024-04-09 12:13:27 -0700

    a class Rectangle that defines a rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -45,6 +45,7 @@ class Rectangle:
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
+        """Return the printable representation of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ("")
 
