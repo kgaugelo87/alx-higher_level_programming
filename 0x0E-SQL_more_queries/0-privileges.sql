7eb860a2814bf8bb2ad3dede079801dfd6b85ed2
Kgaugelo87 <kgaugelo.maile@gmail.com>
2024-06-13 11:52:06 -0700

    lists all privileges of the MySQL users user_0d_1 and user_0d_2

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all the privileges of the sql users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
